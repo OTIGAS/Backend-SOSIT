fba72bfee9d6c10ad6e33a1090d9a1909469f7ac
Tiago Faria Gouvêa <tfariagouvea@gmail.com>
2023-04-28 18:23:03 -0300

    message

--- src/http/controllers/schedules/routes.ts
+++ src/http/controllers/schedules/routes.ts
@@ -5,12 +5,15 @@ import { get } from './get-profile';
 import { FastifyInstance } from 'fastify';
 import { verifyJWT } from '../../middlewares/verify-jwt';
 import { search } from './seach';
+import { deleteSchedule } from './delete'
 
 export async function scheduleRoutes(app: FastifyInstance) {
 
 	app.addHook('onRequest', verifyJWT);
 
 	app.post('/companies/:companyId/schedules', register);
+
+	app.delete('/companies/:companyId/schedules/:scheduleId', deleteSchedule);
 
 	app.get('/companies/:companyId/get-all-schedules', getAll);
 
