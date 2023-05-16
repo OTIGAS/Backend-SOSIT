09cfa6b09efe5c8f48b640ca09356fd2c4b9d79c
Tiago Faria Gouvêa <tfariagouvea@gmail.com>
2023-05-16 13:13:13 -0300

    message

--- frontend-sosit/vite.config.ts
+++ frontend-sosit/vite.config.ts
@@ -1,27 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
-import mock from 'vite-plugin-mock';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-    }),
-    mock({
-      // Define a mock response that adds custom headers to the response
-      '/api/**': {
-        response: {
-          headers: {
-            'Access-Control-Allow-Credentials': 'true',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,OPTIONS,HEAD,PUT,PATCH,POST,DELETE',
-            'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Content-Type, Authorization',
-          },
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://seu-servidor-backend',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '', // Remove o prefixo '/api' da rota
+        },
+        onProxyReq: (proxyReq: any) => {
+          // Adicione os cabeçalhos CORS desejados à requisição
+          proxyReq.setHeader('Access-Control-Allow-Credentials', 'true');
+          proxyReq.setHeader('Access-Control-Allow-Origin', '*');
+          proxyReq.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,HEAD,PUT,PATCH,POST,DELETE');
+          proxyReq.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Content-Type, Authorization');
         },
       },
-    }),
-  ],
+    },
+  },
 });
+
+
+
+
