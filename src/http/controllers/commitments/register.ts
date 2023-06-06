13a8ce3390027c2928fadf0fd4fbdecd8000f18c
OTIGAS <tfariagouvea@gmail.com>
2023-06-06 16:43:30 -0300

    message

--- src/http/controllers/commitments/register.ts
+++ src/http/controllers/commitments/register.ts
@@ -3,51 +3,58 @@ import { z } from 'zod';
 import { makeRegisterCommitmentUseCase } from '@use-case/factories/make-register-commitment-use-case';
 import { UnavailableSchedule } from '@use-case/errors/unavailable-schedule';
 
-export async function register(request: FastifyRequest, response: FastifyReply) {
+interface RegisterRequest extends FastifyRequest {
+  body: {
+    startDateTime: string;
+    endDateTime: string;
+  };
+  params: {
+    scheduleId: string;
+  };
+  user: {
+    sub: string;
+  };
+}
 
-	const customerRegisterParamsSchema = z.object({
-		customerId: z.string().uuid()
-	});
+export async function register(request: RegisterRequest, response: FastifyReply) {
+  const customerRegisterParamsSchema = z.object({
+    customerId: z.string().uuid()
+  });
 
-	const requestData = { customerId: request.user.sub };
+  const requestData = { customerId: request.user.sub };
 
+  const scheduleRegisterParamsSchema = z.object({
+    scheduleId: z.string().uuid()
+  });
 
-	const scheduleRegisterParamsSchema = z.object({
-		scheduleId: z.string().uuid()
-	});
+  const scheduleRegisterBodySchema = z.object({
+    startDateTime: z.string(),
+    endDateTime: z.string(),
+  });
 
-	const scheduleRegisterBodySchema = z.object({
-		startDateTime: z.string(),
-		endDateTime: z.string(),
-	});
+  const { scheduleId } = scheduleRegisterParamsSchema.parse(request.params);
+  const { customerId } = customerRegisterParamsSchema.parse(requestData);
 
-	const { scheduleId } = scheduleRegisterParamsSchema.parse(request.params);
+  const { startDateTime, endDateTime } = scheduleRegisterBodySchema.parse(request.body);
 
-	const { customerId } = customerRegisterParamsSchema.parse(requestData);
+  try {
+    const registerCommitmentUseCase = makeRegisterCommitmentUseCase();
 
-	const {
-		startDateTime,
-		endDateTime
-	} = scheduleRegisterBodySchema.parse(request.body);
+    await registerCommitmentUseCase.execute({
+      customerId,
+      scheduleId,
+      startDateTime,
+      endDateTime
+    });
 
-	try {
-		const registerCommitmnetUseCase = makeRegisterCommitmentUseCase();
+  } catch (err) {
+    if (err instanceof UnavailableSchedule) {
+      return response.status(409).send({
+        message: err.message
+      });
+    }
+    throw err;
+  }
 
-		await registerCommitmnetUseCase.execute({
-			customerId,
-			scheduleId,
-			startDateTime,
-			endDateTime
-		});
-
-	} catch (err) {
-		if (err instanceof UnavailableSchedule) {
-			return response.status(409).send({
-				message: err.message
-			});
-		}
-		throw err;
-	}
-
-	return response.status(201).send();
+  return response.status(201).send();
 }
